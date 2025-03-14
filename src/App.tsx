eed1cb66403dc54b6fdc118a0ba3b3df6ddbe25a
keyng-david <61462535+keyng-david@users.noreply.github.com>
2025-03-14 13:32:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
+import LandingPage from "./components/landing";
 import CryptoRoute from "./components/crypto/CryptoRoute";
 import TaskRoute from "./components/tasks/TaskRoute";
 import routes from "tempo-routes";
@@ -10,7 +11,8 @@ function App() {
     <Suspense fallback={<p>Loading...</p>}>
       <>
         <Routes>
-          <Route path="/" element={<Home />} />
+         <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Home />} />
           <Route path="/crypto" element={<CryptoRoute />} />
           <Route path="/tasks" element={<TaskRoute />} />
         </Routes>
